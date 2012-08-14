997161ad63c41981fe337aaf8eb53feb4ab96a17
Adrian Mowat <adrian.mowat@gmail.com>
2012-08-14 12:05:16 +0100

    added seed file with 3 articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.destroy_all
+
+a = Article.new
+a.title = "Teaching Rails"
+a.body = "I'm teaching Rails in Glasgow today"
+a.save
+
+a2 = Article.new(title: "Weather", body: "It's a bit cloudy today")
+a2.save
+
+a3 = Article.create(title: "Lunch", body: "Lunch will be served at 12")
+a2.body += " and it might rain."
+a2.save

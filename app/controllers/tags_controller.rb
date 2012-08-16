1a81f1f871344ce14fa5ab6fcc5b9bcdefbd0a7c
Adrian Mowat <adrian.mowat@gmail.com>
2012-08-16 09:50:35 +0100

    Added tag index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,14 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def index
+    @tags = Tag.all_tags
+  end
+
+  def destroy
+    tag = Tag.find(params[:id])
+    tag.destroy
+    redirect_to tags_path, :notice => "Tag '#{tag.name}' was deleted" 
+  end
 end
